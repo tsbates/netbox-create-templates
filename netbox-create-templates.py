ca51e8ee7b215327a8f5a85bb0cc53b5d44ba401
Timothy <timaaaay@gmail.com>
2021-10-26 22:12:14 +1030

    Update netbox-create-templates.py

--- netbox-create-templates.py
+++ netbox-create-templates.py
@@ -1,9 +1,8 @@
 import os
-from openpyxl import Workbook, workbook
-from openpyxl.reader.excel import load_workbook, load_workbook
+from openpyxl import load_workbook, load_workbook
 
 cwd = os.getcwd()
-print('[+] Working in %s' % (cwd))
+print('[*] Working in %s' % (cwd))
 
 file = 'IPAM.xlsx'
 
@@ -44,17 +43,67 @@ def create_template_prefixes_and_vlan_roles():
     for row in range(1):
         sheet.append(options)
 
-# TODO
-# Complete template creation for aggregation, RIRs, VRFs, Route Targets etc..
+def create_template_aggregates():
+    sheet = wb['Aggregates']
+    options = ['prefix', 'rir', 'tenant', 'date_added', 'description']
+    for row in range(1):
+        sheet.append(options)
+
+def create_template_rirs():
+    sheet = wb['RIRs']
+    options = ['name', 'slug', 'is_private', 'description']
+    for row in range(1):
+        sheet.append(options)
+
+def create_template_vrfs():
+    sheet = wb['VRFs']
+    options = ['name', 'rd', 'tenant', 'enforce_unique', 'description']
+    for row in range(1):
+        sheet.append(options)
+
+def create_template_route_targets():
+    sheet = wb['Route Targets']
+    options = ['name', 'description', 'tenant']
+    for row in range(1):
+        sheet.append(options)
+
+def create_template_vlans():
+    sheet = wb['VLANs']
+    options = ['site', 'group', 'vid', 'name', 'tenant', 'status', 'role', 'description']
+    for row in range(1):
+        sheet.append(options)
+
+def create_template_vlan_groups():
+    sheet = wb['VLAN Groups']
+    options = ['name', 'slug', 'scope_type', 'scope_id', 'description']
+    for row in range(1):
+        sheet.append(options)
+
+def create_template_services():
+    sheet = wb['Services']
+    options = ['device', 'virtual_name', 'name', 'protocol', 'ports', 'description', 'cf_format']
+    for row in range(1):
+        sheet.append(options)
 
 def save_workbook():
     wb.save(file)
+    print("[*] File %s saved in %s" % (file, cwd))
 
 if __name__ == '__main__':
-    delete_sheets_from_workbook()
-    create_ipam_sheets()
-    create_template_ip_addresses()
-    create_template_ip_ranges()
-    create_template_prefixes()
-    create_template_prefixes_and_vlan_roles()
-    save_workbook()
+    try: 
+        delete_sheets_from_workbook()
+        create_ipam_sheets()
+        create_template_ip_addresses()
+        create_template_ip_ranges()
+        create_template_prefixes()
+        create_template_prefixes_and_vlan_roles()
+        create_template_aggregates()
+        create_template_rirs()
+        create_template_vrfs()
+        create_template_route_targets()
+        create_template_vlans()
+        create_template_vlan_groups()
+        create_template_services()
+        save_workbook()
+    except:
+        print("[!!] The action can't be completed because the file (%s) is open. Close the file and try again." % (file))
